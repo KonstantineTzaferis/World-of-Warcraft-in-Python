d00d389d6df738452c11e72445ecf4daf38d555f
Konstantinos Tzaferis <106535566+KonstantineTzaferis@users.noreply.github.com>
2022-11-08 01:13:49 +0200

    Delete Game.py

--- WoW/Game.py
+++
@@ -1,7 +0,0 @@
-from random import randrange
-from wow import Character
-from Arena import Arena
-
-game = Arena([Character('Orc', attack_speed=2, delay=randrange(0,3)) for i in range(5)],
-            [Character('Night Elf', attack_speed=3, delay=randrange(0,3)) for i in range(3)])
-game.play()
